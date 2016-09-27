7d33b8accacdcef4cfef6a89b3f2f8cf91b08791
Mike Jaffe <mike@streeteasy.com>
2016-09-27 13:31:44 -0400

    making schedule call work with latest rest api

--- lib/appboy/rest/schedule_messages.rb
+++ lib/appboy/rest/schedule_messages.rb
@@ -1,24 +1,29 @@
 module Appboy
   module REST
     class ScheduleMessages < Base
-      attr_reader :app_group_id, :send_at, :messages, :segment_id, :local_timezone
+      attr_reader :app_group_id, :send_at, :messages, :segment_id, :local_timezone, :external_user_ids
 
-      def initialize(app_group_id, send_at:, messages: [], segment_id: nil, local_timezone: false)
+      def initialize(app_group_id, send_at:, messages: [], segment_id: nil, external_user_ids: [], local_timezone: false)
         @app_group_id = app_group_id
         @send_at = send_at
         @messages = messages
         @segment_id = segment_id
+        @external_user_ids = external_user_ids
         @local_timezone = local_timezone
       end
 
       def perform
-        http.post '/messages/schedule', {
+        payload = {
           app_group_id:      app_group_id,
-          segment_ids:               [segment_id],
-          send_at:                   send_at,
-          deliver_in_local_timezone: local_timezone,
+          schedule: {time: send_at, in_local_time: local_timezone},
           messages:                  messages
         }
+        if external_user_ids.size > 0
+          payload.merge!(external_user_ids: external_user_ids)
+        else
+          payload.merge!(segment_id: segment_id)
+        end
+        http.post '/messages/schedule/create', payload
       end
     end
   end
