444011ce1144a1886ca6778e25094406d125c62f
Michael Jaffe <mike@michaeljaffe.com>
2016-09-30 11:55:38 -0400

    adding campaign id to scheduled

--- lib/appboy/rest/schedule_messages.rb
+++ lib/appboy/rest/schedule_messages.rb
@@ -1,15 +1,16 @@
 module Appboy
   module REST
     class ScheduleMessages < Base
-      attr_reader :app_group_id, :send_at, :messages, :segment_id, :local_timezone, :external_user_ids
+      attr_reader :app_group_id, :send_at, :messages, :segment_id, :local_timezone, :external_user_ids, :campaign_id
 
-      def initialize(app_group_id, send_at:, messages: [], segment_id: nil, external_user_ids: [], local_timezone: false)
+      def initialize(app_group_id, send_at:, messages: [], external_user_ids: [], campaign_id: nil, segment_id: nil,  local_timezone: false)
         @app_group_id = app_group_id
         @send_at = send_at
         @messages = messages
         @segment_id = segment_id
         @external_user_ids = external_user_ids
         @local_timezone = local_timezone
+        @campaign_id = campaign_id
       end
 
       def perform
@@ -23,6 +24,7 @@ module Appboy
         else
           payload.merge!(segment_id: segment_id)
         end
+        payload.merge!(campaign_id: campaign_id) unless campaign_id.nil?
         http.post '/messages/schedule/create', payload
       end
     end
