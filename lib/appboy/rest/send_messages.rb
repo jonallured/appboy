ded3aa0f5d8b5405411855d2836ce855b76f95a9
Michael Jaffe <mike@michaeljaffe.com>
2016-09-30 11:51:41 -0400

    adding campaign id

--- lib/appboy/rest/send_messages.rb
+++ lib/appboy/rest/send_messages.rb
@@ -1,12 +1,13 @@
 module Appboy
   module REST
     class SendMessages < Base
-      attr_reader :app_group_id, :messages, :external_user_ids, :segment_id
+      attr_reader :app_group_id, :messages, :external_user_ids, :segment_id, :campaign_id
 
-      def initialize(app_group_id, messages: [], external_user_ids: [], segment_id: nil)
+      def initialize(app_group_id, messages: [], external_user_ids: [], campaign_id: nil, segment_id: nil)
         @app_group_id = app_group_id
         @messages = messages
         @external_user_ids = external_user_ids
+        @campaign_id = campaign_id
         @segment_id = segment_id
       end
 
@@ -15,6 +16,7 @@ module Appboy
           app_group_id:      app_group_id,
           messages:          messages,
           external_user_ids: external_user_ids,
+          campaign_id: campaign_id,
           segment_ids:       [segment_id].compact
         }
       end
