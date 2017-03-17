cfc943d8b7f4671ff9c27e4d24d116d6a10f67b8
Vladimir Stanishev <vladimirs@streeteasy.com>
2017-03-17 17:30:27 -0400

    add logging to appboy

--- lib/appboy/rest/send_messages.rb
+++ lib/appboy/rest/send_messages.rb
@@ -3,22 +3,26 @@ module Appboy
     class SendMessages < Base
       attr_reader :app_group_id, :messages, :external_user_ids, :segment_id, :campaign_id
 
-      def initialize(app_group_id, messages: [], external_user_ids: [], campaign_id: nil, segment_id: nil)
+      def initialize(app_group_id, messages: [], external_user_ids: [], campaign_id: nil, segment_id: nil, logger: nil)
         @app_group_id = app_group_id
         @messages = messages
         @external_user_ids = external_user_ids
         @campaign_id = campaign_id
         @segment_id = segment_id
+        @send_uri = '/messages/send'
+        @logger = logger
       end
 
       def perform
-        http.post '/messages/send', {
-          app_group_id:      app_group_id,
-          messages:          messages,
-          external_user_ids: external_user_ids,
-          campaign_id: campaign_id,
-          segment_ids:       [segment_id].compact
+        payload = {
+            app_group_id:      app_group_id,
+            messages:          messages,
+            external_user_ids: external_user_ids,
+            campaign_id: campaign_id,
+            segment_ids:       [segment_id].compact
         }
+        @logger.info("#{self.class.name}") { "http.post: #{@send_uri} payload: #{payload.to_s}"} unless @logger.nil?
+        http.post @send_uri, payload
       end
     end
   end
