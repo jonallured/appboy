d73b81eed99c2adaf0d99714666008ec57244428
Eric Agnew <wenga.eric@gmail.com>
2017-06-27 10:45:33 -0500

    Add delete_users api

--- lib/appboy/api.rb
+++ lib/appboy/api.rb
@@ -1,5 +1,6 @@
 require 'appboy/deprecated'
 require 'appboy/endpoints/track_users'
+require 'appboy/endpoints/delete_users'
 require 'appboy/endpoints/send_messages'
 require 'appboy/endpoints/schedule_messages'
 require 'appboy/endpoints/email_status'
@@ -9,7 +10,9 @@ module Appboy
     include Appboy::Deprecated
 
     include Appboy::Endpoints::TrackUsers
+    include Appboy::Endpoints::DeleteUsers
     include Appboy::Endpoints::SendMessages
+    include Appboy::Endpoints::TriggeredMessages
     include Appboy::Endpoints::ScheduleMessages
     include Appboy::Endpoints::EmailStatus
 
