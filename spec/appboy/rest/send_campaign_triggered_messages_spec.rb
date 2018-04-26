1f56bd0647aa2d45f74208676cb53ce4d33c7134
Eric Agnew <wenga.eric@gmail.com>
2018-04-26 09:58:55 -0500

    Add campaign endpoint and method

---
+++ spec/appboy/rest/send_campaign_triggered_messages_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe Appboy::REST::SendCampaignTriggeredMessages do
+  let(:http) { double(:http) }
+
+  let(:payload) do
+    {
+      audience: :audience,
+      broadcast: :broadcast,
+      campaign_id: :campaign_id,
+      recipients: :recipients,
+      send_id: :send_id,
+      trigger_properties: :trigger_properties
+    }
+  end
+
+  let(:api_key) { :api_key }
+
+  subject do
+    described_class.new(
+      api_key,
+      audience: :audience,
+      broadcast: :broadcast,
+      campaign_id: :campaign_id,
+      recipients: :recipients,
+      send_id: :send_id,
+      trigger_properties: :trigger_properties
+    )
+  end
+
+  before { subject.http = http }
+
+  it 'makes an http call to the send messages endpoint' do
+    expect_send_messages_http_call
+
+    subject.perform
+  end
+
+  def expect_send_messages_http_call
+    expect(http).to receive(:post).with '/campaigns/trigger/send', {
+      api_key: :api_key,
+      audience: :audience,
+      broadcast: :broadcast,
+      campaign_id: :campaign_id,
+      recipients: :recipients,
+      send_id: :send_id,
+      trigger_properties: :trigger_properties
+    }
+  end
+end
