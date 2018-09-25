78f49db1fa3fa05cb8268f3e38c669a574660bbe
Adrian Pacała <adrianlukaszpacala@gmail.com>
2018-09-25 16:48:33 +0100

    Add trigger campaign endpoint

--- lib/appboy/rest/trigger_campaign.rb
+++ lib/appboy/rest/trigger_campaign.rb
@@ -1,29 +1,25 @@
 module Appboy
   module REST
     class TriggerCampaign < Base
-      attr_reader :api_key, :audience, :broadcast, :campaign_id,
-                  :recipients, :send_id, :trigger_properties
+      attr_accessor :app_group_id, :campaign_id, :external_user_ids, :trigger_properties
 
-      def initialize(api_key, options = {})
-        @api_key            = api_key
-        @audience           = options[:audience]
-        @broadcast          = options[:broadcast] || false
-        @campaign_id        = options[:campaign_id]
-        @recipients         = options[:recipients]
-        @send_id            = options[:send_id]
-        @trigger_properties = options[:trigger_properties]
+      def initialize(app_group_id, campaign_id:, external_user_ids: [], trigger_properties: {})
+        @app_group_id = app_group_id
+        @campaign_id = campaign_id
+        @external_user_ids = external_user_ids
+        @trigger_properties = trigger_properties
       end
 
       def perform
-        http.post '/campaigns/trigger/send', {
-          api_key:            api_key,
-          audience:           audience,
-          broadcast:          broadcast,
-          campaign_id:        campaign_id,
-          recipients:         recipients,
-          send_id:            send_id,
-          trigger_properties: trigger_properties
-        }.compact
+        http.post(
+          '/campaigns/trigger/send',
+          app_group_id: app_group_id,
+          campaign_id: campaign_id,
+          recipients: {
+            external_user_ids: external_user_ids,
+          },
+          trigger_properties: trigger_properties,
+        )
       end
     end
   end
--- spec/appboy/rest/trigger_campaign_spec.rb
+++ spec/appboy/rest/trigger_campaign_spec.rb
@@ -3,48 +3,33 @@ require 'spec_helper'
 describe Appboy::REST::TriggerCampaign do
   let(:http) { double(:http) }
 
-  let(:payload) do
+  let :payload do
     {
-      audience: :audience,
-      broadcast: :broadcast,
+      app_group_id: :app_group_id,
       campaign_id: :campaign_id,
-      recipients: :recipients,
-      send_id: :send_id,
-      trigger_properties: :trigger_properties
+      recipients: {
+        external_user_ids: :external_user_ids,
+      },
+      trigger_properties: :trigger_properties,
     }
   end
 
-  let(:api_key) { :api_key }
+  let(:app_group_id) { :app_group_id }
 
   subject do
     described_class.new(
-      api_key,
-      audience: :audience,
-      broadcast: :broadcast,
+      app_group_id,
       campaign_id: :campaign_id,
-      recipients: :recipients,
-      send_id: :send_id,
-      trigger_properties: :trigger_properties
+      external_user_ids: :external_user_ids,
+      trigger_properties: :trigger_properties,
     )
   end
 
   before { subject.http = http }
 
-  it 'makes an http call to the send messages endpoint' do
-    expect_send_messages_http_call
+  it 'makes an HTTP call to the trigger campaign endpoint' do
+    expect(http).to receive(:post).with('/campaigns/trigger/send', payload)
 
     subject.perform
-  end
-
-  def expect_send_messages_http_call
-    expect(http).to receive(:post).with '/campaigns/trigger/send', {
-      api_key: :api_key,
-      audience: :audience,
-      broadcast: :broadcast,
-      campaign_id: :campaign_id,
-      recipients: :recipients,
-      send_id: :send_id,
-      trigger_properties: :trigger_properties
-    }
   end
 end
