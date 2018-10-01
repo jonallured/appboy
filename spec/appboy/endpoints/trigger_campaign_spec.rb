96b66d5f7edb6b484aca09929adfebe9a6fdce5c
Adrian Pacała <adrianlukaszpacala@gmail.com>
2018-10-01 10:42:41 +0100

    [#159170146] Add trigger campaign endpoint

---
+++ spec/appboy/endpoints/trigger_campaign_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+# Force autoload.
+Appboy::API
+
+class API
+  include Appboy::Endpoints::TriggerCampaign
+
+  def app_group_id
+    :app_group_id
+  end
+end
+
+describe Appboy::Endpoints::TriggerCampaign do
+  let(:api) { API.new }
+  let(:trigger_campaign_service) { double(:trigger_campaign_service) }
+
+  before { expect(api).to receive(:trigger_campaign_service).and_return(trigger_campaign_service) }
+
+  describe '#trigger_campaign_service' do
+    let(:payload) {{
+      campaign_id: 1,
+      external_user_ids: [],
+      trigger_properties: {},
+    }}
+
+    subject(:trigger_campaign!) { api.trigger_campaign(payload) }
+
+    it 'triggers campaign' do
+      expect(trigger_campaign_service).to receive(:perform).with(:app_group_id, payload)
+
+      trigger_campaign!
+    end
+  end
+end
